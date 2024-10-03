92eb900f116dc55dc270787dd0fb7b1bf3d064e1
Hung24855 <hung24855@gmail.com>
2024-10-03 22:28:00 +0700

    merge firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,6 +6,7 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY_FIRE_BASE,
     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN_FIRE_BASE,
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL_FIRE_BASE,
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID_FIRE_BASE,
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET_FIRE_BASE,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID_FIRE_BASE,
