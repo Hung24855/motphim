592eeee088904098997766cfb82f3daf675e8b8d
Hung24855 <hung24855@gmail.com>
2024-10-28 16:33:29 +0700

    Update

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,25 +1,29 @@
 import { Pool } from "pg";
 
+// export const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: Number(process.env.DB_PORT),
+// });
+
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+    connectionString: process.env.POSTGRES_URL
 });
 
 export default async function DB_Connect() {
-  await pool.connect((err, client, release) => {
-    if (err) {
-      console.error("Lỗi khi connect đến postgres: ", err.stack);
-    }
+    pool.connect((err, client, release) => {
+        if (err) {
+            console.error("Lỗi khi connect đến postgres: ", err.stack);
+        }
 
-    client?.query("SELECT NOW()", (err, result) => {
-      release();
-      if (err) {
-        console.error("Lỗi khi connect đến postgres: ", err.stack);
-      }
-      console.log("Kết nối cơ sở dữ liệu thành công! ", result.rows[0].now);
+        client?.query("SELECT NOW()", (err, result) => {
+            release();
+            if (err) {
+                console.error("Lỗi khi connect đến postgres: ", err.stack);
+            }
+            console.log("Kết nối cơ sở dữ liệu thành công! ", result.rows[0].now);
+        });
     });
-  });
 }
