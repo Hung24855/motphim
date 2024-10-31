1e58f66bd147f7fde282d606453ff765d9ac775a
Hung24855 <hung24855@gmail.com>
2024-10-31 16:32:22 +0700

    fix bug

--- src/app/api/middleware/index.ts
+++ src/app/api/middleware/index.ts
@@ -1,12 +1,17 @@
 import { pool } from "@/database/connect";
 import { getToken } from "next-auth/jwt";
+import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 const secret = process.env.NEXTAUTH_SECRET ?? "AUTH_SECRET";
 const CheckAdmin = async (request: NextRequest) => {
     try {
-        console.log("🚀 ~ CheckAdmin ~ secret:", secret);
+
+        const cookie = cookies();
+        console.log("🚀 ~ CheckAdmin ~ cookie:", cookie)
         const token_product = await getToken({ req: request, secret: secret, salt: "__Secure-authjs.session-token" });
+        const local_token = await getToken({ req: request, secret: secret, salt: "authjs.session-token" });
         console.log("🚀 ~ CheckAdmin ~ token_product:", token_product);
+        console.log("🚀 ~ CheckAdmin ~ local_token:", local_token)
 
         const check_admin_in_DB = await pool.query("SELECT role FROM users WHERE id = $1", [token_product?.id]);
 
