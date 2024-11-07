9332426707f094cad8f64c82293670aa6eb551e8
Hung24855 <hung24855@gmail.com>
2024-11-07 15:10:02 +0700

    Update

--- src/app/api/cronjob/route.ts
+++ src/app/api/cronjob/route.ts
@@ -1,9 +1,9 @@
 import { RouterHandler } from "../router.handler";
+export const revalidate = 0;
 export async function GET() {
     return RouterHandler({
         async mainFc(pool) {
             console.log("Cron job running at ", new Date());
-
             return {
                 message: "Cron job running at " + new Date(),
                 data: []
