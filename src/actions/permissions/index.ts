3fe8fa916f7a1ff8ca1895d7460a0d527618ef13
Hung24855 <hung24855@gmail.com>
2024-11-07 15:19:39 +0700

    fix conflic

--- src/actions/permissions/index.ts
+++ src/actions/permissions/index.ts
@@ -6,14 +6,9 @@ const IS_DEVELOPMENT = process.env.DEVELOPMENT === "development";
 export const Permissions = async ({ user_id, role }: { user_id: string; role: "admin" | "user" }) => {
     try {
         const cookie = cookies();
-<<<<<<< HEAD
 
         const session_product = cookie.get("__Secure-authjs.session-token")?.value;
         if (session_product) {
-=======
-        const session = cookie.get(IS_DEVELOPMENT ? "authjs.session-token" : "__Secure-authjs.session-token")?.value;
-        if (!session) {
->>>>>>> 9332426707f094cad8f64c82293670aa6eb551e8
             redirect("/");
         }
 
@@ -21,11 +16,7 @@ export const Permissions = async ({ user_id, role }: { user_id: string; role: "a
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
-<<<<<<< HEAD
                 cookie: "__Secure-authjs.session-token=" + session_product
-=======
-                cookie: (IS_DEVELOPMENT ? "authjs.session-token=" : "__Secure-authjs.session-token=") + session
->>>>>>> 9332426707f094cad8f64c82293670aa6eb551e8
             },
             body: JSON.stringify({ role })
         }).then((res) => res.json());
